7c92a6d6e40a890d2daf51547f7da7e760c271c5
marcelotsfilho <marcelo.estudo.cefet@gmail.com>
2025-08-28 14:07:06 -0300

    teste

---
+++ iniciante/numeros-pares.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+/*
+Titulo: Numeros pares - BEE 1053
+link: https://judge.beecrowd.com/pt/problems/view/1053
+*/
+
+using namespace std;
+
+int main(){
+
+    for(int i = 2; i <=100; i++){
+        if(i%2 == 0){
+            cout << i << endl;
+        }
+    }
+
+    return 0;
+}
